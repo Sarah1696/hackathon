7b5cbc14a56d62ae53b01c025dc05782da50bffb
elmadi.sarah <elmadi.sarah@gmail.com>
2025-08-04 15:11:50 +0200

    model controller routes Comments + modif database et server.js

--- backend/modules/comments/model.js
+++ backend/modules/comments/model.js
@@ -1,1 +1,15 @@
-import database from '../../database.js'
+import db_sql from '../../database.js'
+
+export default class CommentsModel {
+    static async getComments() {
+        const response = await db_sql.query('SELECT * FROM comments')
+        return response
+    }
+
+    static async postComments({ idea_id, user_id, content }) {
+        const query = `INSERT INTO comments (idea_id, user_id, content, created_at) VALUES (?, ?, ?, NOW())`;
+        const values = [idea_id, user_id, content];
+        const response = await db_sql.query(query, values);
+        return response;
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from 'cors'
 import bodyParser from "body-parser";
 import path from 'path'
+import commentsRoutes from './modules/comments/routes.js'
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/api/comments', commentsRoutes)
 
 app.use (bodyParser.urlencoded({ extended : true}))
 
