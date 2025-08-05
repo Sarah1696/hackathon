9b44290a21fa4e79d5f91abc3204dc2dd0a2e362
elmadi.sarah <elmadi.sarah@gmail.com>
2025-08-05 11:48:28 +0200

    home

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Header = () => {
     return (
           <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div style={{ justifyContent: "space-around" }} className="container-fluid  ">              
+            <div style={{ justifyContent: "space-around" }} className="container-fluid ">              
                <Link className="navbar-brand" to="/">Boite à idée</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
